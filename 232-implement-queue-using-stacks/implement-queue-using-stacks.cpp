9a1dce3391426d055355205ce78b8f5cb9d7bc76
Vikash rao <vikashrao6663@gmail.com>
2026-06-10 12:11:18 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (7.69%) - LeetSync

---
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.cpp
@@ -0,0 +1,50 @@
+class MyQueue {
+public:
+stack<int>st;
+stack<int>helper;
+    MyQueue() {
+        
+    }
+    
+    void push(int x) {
+        st.push(x);
+    }
+    
+    int pop() {
+        while(st.size()>0){
+            helper.push(st.top());
+            st.pop();
+            }
+       int x=helper.top();
+        helper.pop();
+        while(helper.size()>0){
+            st.push(helper.top());
+            helper.pop();
+        }  
+        return x;  
+        
+    }
+    
+    int peek() {
+        while(st.size()>0){
+            helper.push(st.top());
+            st.pop();
+            }
+        int x=helper.top();
+        while(helper.size()>0){
+            st.push(helper.top());
+            helper.pop();
+        } 
+        return x; 
+        
+    }
+    
+    bool empty() {
+        if(st.size()==0)return true;
+        else 
+        return  false;
+        
+    }
+
+};
+
