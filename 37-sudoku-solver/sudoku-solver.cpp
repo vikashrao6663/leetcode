8a2fba74b9e7777cc43fdc9996ac13fceb3959db
Vikash rao <vikashrao6663@gmail.com>
2026-06-17 00:42:00 +0530

    Time: 227 ms (78.43%) | Memory: 8.8 MB (37.65%) - LeetSync

---
+++ 37-sudoku-solver/sudoku-solver.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+bool issafe(vector<vector<char>>& board,int row,int col,char dig){
+    // horizontal
+    for(int j =0;j<9;j++){
+        if(board[row][j]==dig){
+            return false;
+        }
+    }
+    // column
+    for(int i=0;i<9;i++){
+        if(board[i][col]==dig){
+            return false;
+        }
+    }
+    //grid
+    int srow =(row/3)*3;
+    int scol =(col/3)*3;
+    for(int i =srow;i<=srow+2;i++){
+        for(int j =scol;j<=scol+2;j++){
+            if(board[i][j]==dig){
+                return false;
+            }
+        }
+    }
+    return true;
+
+}
+bool helper(vector<vector<char>> & board,int row,int col){
+    if(row == 9){
+        return true;
+    }
+    int nextrow=row,nextcol=col+1;
+    if(nextcol==9){
+        nextrow=row+1;
+        nextcol=0;
+    }
+    if(board[row][col]!='.'){
+        return helper(board,nextrow,nextcol);
+    }
+    for(char dig='1';dig<='9';dig++){
+        if(issafe(board,row,col,dig)){
+            board[row][col] = dig;
+            if(helper(board,nextrow,nextcol)){
+                return true;
+            }
+            board[row][col]='.';
+        }
+    }
+    return false;
+    }
+    void solveSudoku(vector<vector<char>>& board) {
+        helper(board,0,0);
+
+    }
+};
