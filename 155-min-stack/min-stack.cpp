6c5495c7e1184c13b704022aeb3b3e194fb607f3
Vikash rao <vikashrao6663@gmail.com>
2026-06-17 10:52:04 +0530

    Time: 74 ms (6.60%) | Memory: 151.2 MB (17.91%) - LeetSync

---
+++ 155-min-stack/min-stack.cpp
@@ -0,0 +1,36 @@
+class MinStack {
+public:
+
+    stack<int> st;
+    stack<int> minst;
+
+    MinStack() {
+
+    }
+
+    void push(int val) {
+
+        st.push(val);
+
+        if(minst.empty() || val <= minst.top()) {
+            minst.push(val);
+        }
+    }
+
+    void pop() {
+
+        if(st.top() == minst.top()) {
+            minst.pop();
+        }
+
+        st.pop();
+    }
+
+    int top() {
+        return st.top();
+    }
+
+    int getMin() {
+        return minst.top();
+    }
+};
