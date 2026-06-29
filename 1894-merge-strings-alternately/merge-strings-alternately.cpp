038459e013754e90cef063a9c4a8481b803eaae5
Vikash rao <vikashrao6663@gmail.com>
2026-06-29 13:50:08 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (31.76%) - LeetSync

---
+++ 1894-merge-strings-alternately/merge-strings-alternately.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+     string ans;
+     int i = 0;
+
+     while(i<word1.size() && i< word2.size()){
+        ans +=word1[i];
+        ans +=word2[i];
+        i++;
+     }   
+     while(i<word1.size())
+     {ans +=word1[i];
+     i++;}
+
+     while(i<word2.size())
+     {ans +=word2[i];
+     i++;
+     }
+
+     return ans;
+    }
+};
