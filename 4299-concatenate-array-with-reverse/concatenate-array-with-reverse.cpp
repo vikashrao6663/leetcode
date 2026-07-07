b053b4802bf40cdd89d1fd21cba1dc3a7dbd2b2e
Vikash rao <vikashrao6663@gmail.com>
2026-07-07 21:07:50 +0530

    Time: 2 ms (28.07%) | Memory: 30.9 MB (33.07%) - LeetSync

--- 4299-concatenate-array-with-reverse/concatenate-array-with-reverse.cpp
+++ 4299-concatenate-array-with-reverse/concatenate-array-with-reverse.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
     vector<int> concatWithReverse(vector<int>& nums) {
-    int n = nums.size();
-       for(int i=n-1;i>=0;i--){
-    nums.push_back(nums[i]);
-       }
-       return nums;
+
+        for(int i=nums.size()-1;i>=0;i--){
+            nums.push_back(nums[i]);
+        }
+        return nums;
+        
     }
 };
