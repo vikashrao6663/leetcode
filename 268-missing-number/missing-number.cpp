ffb7c3ebd8a21c6700396873206dde3d621ae6ac
Vikash rao <vikashrao6663@gmail.com>
2026-07-10 14:16:55 +0530

    Time: 14 ms (10.16%) | Memory: 21.9 MB (38.72%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -1,15 +1,12 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        int n = nums.size();
-if(nums[0] != 0) return 0;
-        for(int i = 0; i < n - 1; i++) {
-            if(nums[i] + 1 != nums[i + 1]) {
-                return nums[i] + 1;
-            }
-        }
-
-        return n;
+        sort(nums.begin(),nums.end());
+        int i;
+     for(i =0;i<nums.size();i++){
+        if(nums[i]!=i) return i;
+        
+     }   
+     return i;
     }
 };
