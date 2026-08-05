39beeaf8bcf4ad31daf6f10bc354fd89437a4671
Vikash rao <vikashrao6663@gmail.com>
2026-08-06 01:48:01 +0530

    Time: 0 ms (100.00%) | Memory: 11.1 MB (15.68%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
-    void preorder(TreeNode* root, vector<int>& ans){
-        if(root == NULL) return;
-
-        ans.push_back(root->val);  // Root
-        preorder(root->left, ans); // Left
-        preorder(root->right, ans);// Right
-    }
-
     vector<int> preorderTraversal(TreeNode* root) {
-        vector<int> ans;
-        preorder(root, ans);
-        return ans;
+      vector<int>ans;
+      stack<TreeNode*>st;
+      if(root!=NULL) st.push(root);
+      while(st.size()>0){
+        TreeNode*temp=st.top();
+        st.pop();
+        ans.push_back(temp->val);
+        if(temp->right!=NULL) st.push(temp->right);
+         if(temp->left!=NULL) st.push(temp->left);
+
+      }  
+      return ans;
     }
 };
