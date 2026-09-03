b434c375a86dc311e8e6df160cae84f8b4c168be
Vikash rao <vikashrao6663@gmail.com>
2026-09-04 01:21:30 +0530

    Time: 4 ms (13.10%) | Memory: 10.3 MB (84.72%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -1,28 +1,36 @@
 class Solution {
 public:
-void getallsubset(vector<int>& nums,vector<int>& ans,int i, vector<vector<int>>& allsubset){
-    if(i == nums.size()){
-    allsubset.push_back(ans);
-    return;
+    void getallsubset(vector<int>& nums, vector<int>& ans, int i,
+                      vector<vector<int>>& allsubset) {
+
+        if (i == nums.size()) {
+            allsubset.push_back(ans);
+            return;
+        }
+
+        // include
+        ans.push_back(nums[i]);
+        getallsubset(nums, ans, i + 1, allsubset);
+        ans.pop_back();
+
+        // skip duplicates
+        int idx = i + 1;
+        while (idx < nums.size() && nums[idx] == nums[i]) {
+            idx++;
+        }
+
+        // exclude
+        getallsubset(nums, ans, idx, allsubset);
     }
-    ans.push_back(nums[i]);
-    getallsubset(nums,ans,i+1,allsubset);
 
-    ans.pop_back();
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
 
-int idx=i+1;
-    while(idx < nums.size() && nums[idx]==nums[i]) idx++;
+        vector<vector<int>> allsubset;
+        vector<int> ans;
 
-    getallsubset(nums,ans,idx,allsubset);
-}
-    vector<vector<int>>  subsetsWithDup(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        vector<vector<int>>allsubset;
-        vector<int> ans; 
+        getallsubset(nums, ans, 0, allsubset);
 
-       getallsubset(nums,ans,0,allsubset);
-
-
-    return allsubset;    
+        return allsubset;
     }
 };
